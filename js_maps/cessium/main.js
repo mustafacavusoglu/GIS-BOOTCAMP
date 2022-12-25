af1c4fa8b87d257afa93c9882565b69cfe0166e0
Mustafa Çavuşoğlu <mustafacavussoglu@gmail.com>
2022-12-25 15:50:46 +0300

    renklendirme eklendi

--- js_maps/cessium/main.js
+++ js_maps/cessium/main.js
@@ -1,12 +1,12 @@
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0NGExYjJiMC1iMWI4LTQxZmYtOWQ3Yi1kNGRhZmIxMzc1NjciLCJpZCI6ODg4LCJpYXQiOjE2NzE2Mjg3MjJ9.sHXG04RRdVzBT4uBz60aFCiksRk57v_MbKVbhAQQGxk"
+
 let viewer = new Cesium.Viewer('cesiumContainer', {
     // terrainProvider: Cesium.createWorldTerrain()
     animation: false,
     baseLayerPicker: false,
     fullscreenButton: false,
     geocoder: false,
-    infoBox: false,
     sceneModePicker: false,
-    selectionIndicator: false,
     timeline: false,
     navigationHelpButton: false,
     navigationInstructionsInitiallyVisible: false,
@@ -14,23 +14,25 @@ let viewer = new Cesium.Viewer('cesiumContainer', {
     shouldAnimate: true
 });
 
+viewer.terrainProvider = Cesium.createWorldTerrain();
+
 
 
 
 document.getElementsByClassName('cesium-viewer-bottom')[0].style.display = 'none'
 
 viewer.camera.setView({
-    destination : Cesium.Cartesian3.fromDegrees(28.96, 41.01, 8000),
+    destination: Cesium.Cartesian3.fromDegrees(28.96, 41.01, 8000),
     orientation: {
-        heading : Cesium.Math.toRadians(0.0), 
-        pitch : Cesium.Math.toRadians(-90),   
-        roll : 0                            
+        heading: Cesium.Math.toRadians(0.0),
+        pitch: Cesium.Math.toRadians(-90),
+        roll: 0
     }
 });
 
 
 
-let mapboxButton = document.getElementById('mapbox').onclick = ()=> {
+let mapboxButton = document.getElementById('mapbox').onclick = () => {
     viewer.scene.imageryLayers.removeAll();
     let MAPBOX_ACCESS_TOKEN = "pk.eyJ1Ijoic2VsYWhhdHRpbiIsImEiOiJjbGJ4anh4NDgwN3BwM25ybWwzbG9sdDJpIn0.GjNP1CgfPTcojfCI95vsCA";
     let MAPBOX_STYLE_ID = "cjqglqul3i0cc2sqi6ixvmo3p";
@@ -41,10 +43,10 @@ let mapboxButton = document.getElementById('mapbox').onclick = ()=> {
     viewer.imageryLayers.addImageryProvider(customImageryProvider);
 }
 
-let esriButton = document.getElementById('esri').onclick = ()=> {
+let esriButton = document.getElementById('esri').onclick = () => {
     viewer.scene.imageryLayers.removeAll();
     const esri = new Cesium.ArcGisMapServerImageryProvider({
-        url : 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+        url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
     });
 
     viewer.imageryLayers.addImageryProvider(esri);
@@ -68,12 +70,103 @@ let topkapiButton = document.getElementById('topkapi').onclick = () => {
 
 let flyTo = (lat, lon, alt, duration) => {
     viewer.camera.flyTo({
-        destination : Cesium.Cartesian3.fromDegrees(lat, lon, alt),
-        orientation : {
-            heading : Cesium.Math.toRadians(0),
-            pitch : Cesium.Math.toRadians(-90.0),
-            roll : 0.0
+        destination: Cesium.Cartesian3.fromDegrees(lat, lon, alt),
+        orientation: {
+            heading: Cesium.Math.toRadians(0),
+            pitch: Cesium.Math.toRadians(-90.0),
+            roll: 0.0
         },
         duration: duration
     });
+}
+
+/* const osmBuildingsTileset = Cesium.createOsmBuildings();
+viewer.scene.primitives.add(osmBuildingsTileset); */
+
+
+let addOsmBuilding = document.getElementById('osm').onclick = () => {
+    viewer.scene.primitives.removeAll()
+
+    const osmBuildingsTileset = Cesium.createOsmBuildings();
+    viewer.scene.primitives.add(osmBuildingsTileset);
+
+}
+
+let lod1Tileset = new Cesium.Cesium3DTileset({
+    url: Cesium.IonResource.fromAssetId(1452992),
+})
+let addLod1 = document.getElementById('lod1').onclick = () => {
+    viewer.scene.primitives.removeAll()
+    viewer.scene.primitives.add(lod1Tileset)
+    document.getElementById('style').style.display = 'block'
+}
+
+
+function getRadioType() {
+    let styleBody = document.getElementById('styleBodyContainer')
+    var select = document.querySelector('input[name="style"]:checked').value;
+    if (select == 1) {
+        styleBody.innerHTML = `
+        <div id="singleColorContainer">
+            <div>
+                <input type="color" id="singleColorPalette">
+            </div>
+            <div>
+                <button class="button" id="singleColorSubmit" onclick=singleColorSubmit() >Uygula</button>
+            </div>
+        </div>
+`
+    }
+    else if(select == 2) {
+        styleBody.innerHTML = `
+        <div id="multiColorContainer">
+        <div>
+            <select id="multiColorSelect">
+                <option value="1">İşyeri Sayısı</option>
+                <option value="2">Konut Sayısı</option>
+                <option value="3">Mahalle</option>
+              </select>
+        </div>
+        <div>
+
+         <div>
+            <button class="button" id="multiColorSubmit" onclick = multiColorSubmit()>Uygula</button>
+        </div>
+
+    </div>
+        `
+    }
+}
+
+
+const singleColorSubmit = () => {
+    let value =document.getElementById('singleColorPalette').value;
+    let color = `color('${value}')`
+
+    lod1Tileset.style = new Cesium.Cesium3DTileStyle({
+        color: {
+          conditions: [
+            // ["true", "color('#ffffff')"],
+            ["true",color],
+          ],
+        },
+      });
+}
+
+const multiColorSubmit = () => {
+    let value =document.getElementById('multiColorSelect').value;
+
+
+    lod1Tileset.style = new Cesium.Cesium3DTileStyle({
+        color: {
+          conditions: [
+            ["${ISYERI_SAY} > 100", "color('#d65c5c')"],
+            ["${ISYERI_SAY} > 50 ", "color('red')"],
+            ["${ISYERI_SAY} > 30 ", "color('orange')"],
+            ["${ISYERI_SAY} > 15 ", "color('yellow')"],
+            ["${ISYERI_SAY} > 1 ", "color('pink')"],
+            ["true", "color('white')"],
+          ],
+        },
+      });
 }
